cd8b334a45eb6e10f3b50022a681f73f911a0a5b
pdvladking <pdvladking@gmail.com>
2025-07-05 12:26:30 +0545

    Initial commit: Corekit setup

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react", "prettier"],
+  rules: {
+    "react/prop-types": "off",
+    "prettier/prettier": "error",
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-extrabold text-indigo-700 drop-shadow-md text-center">
+        Welcome to CoreKit 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
