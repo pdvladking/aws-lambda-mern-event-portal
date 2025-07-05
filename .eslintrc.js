b5e0a0a448aa950606241e3dd91ab3aeb6c94648
pdvladking <pdvladking@gmail.com>
2025-07-05 12:59:53 +0545

    Deploy refined React frontend

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   env: { browser: true, es2021: true },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react", "prettier"],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react', 'prettier'],
   rules: {
-    "react/prop-types": "off",
-    "prettier/prettier": "error",
+    'react/prop-types': 'off',
+    'prettier/prettier': 'error',
   },
 };
