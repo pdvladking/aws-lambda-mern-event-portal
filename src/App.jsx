2d08c38c476d0f3a84ff779065bf4d37fea8a881
pdvladking <pdvladking@gmail.com>
2025-07-05 13:51:54 +0545

    Initial React + Tailwind + Prettier setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-extrabold text-purple-500 drop-shadow-lg">
+        Hello Raj, Tailwind is locked in 👑
+      </h1>
+    </div>
+  );
+}
+
+export default App;
